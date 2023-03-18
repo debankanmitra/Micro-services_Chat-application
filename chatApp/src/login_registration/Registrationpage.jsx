d2ad39f4db0a3cb3fe9eb26aeaeead42902ce120
debankanmitra <debankanmitra@gmail.com>
2023-03-18 16:17:13 +0530

    regpage

---
+++ chatApp/src/login_registration/Registrationpage.jsx
@@ -0,0 +1,19 @@
+import React from 'react'
+import styled from 'styled-components'
+
+function Registrationpage() {
+    const Div2 = styled.div `
+      width: 50vw;
+      height: 100vh;
+      background: #FCE0D4;
+    `;
+  return (
+    <div>
+        <Div2>
+        <Image src='chat.svg'/>
+        </Div2>
+    </div>
+  )
+}
+
+export default Registrationpage
