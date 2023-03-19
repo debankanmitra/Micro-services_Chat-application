feab7d44a2d60d987c41013f719bdd3451c00ead
debankanmitra <debankanmitra@gmail.com>
2023-03-19 22:54:06 +0530

    created

--- chatApp/src/login_registration/Registrationpage.jsx
+++ chatApp/src/login_registration/Registrationpage.jsx
@@ -1,18 +1,39 @@
 import React from 'react'
 import styled from 'styled-components'
+import Login from './Form';
 
 function Registrationpage() {
-    const Div2 = styled.div `
-      width: 50vw;
-      height: 100vh;
+  const Div = styled.div`
+        width: 100vw;
+        height:100vh;
+        border: 1px solid red;
+        display:grid;
+        grid-template-columns:1fr 1fr;
+        background: #F5F4F4;
+  
+  `
+  const Div1 = styled.div`
+    display: flex;
+    justify-content: center;
+    align-items: center;
+  `
+  const Div2 = styled.div`
+      /* width: 50vw;
+      height: 100vh; */
+      display: flex;
+      justify-content: center;
+      align-items: center;
       background: #FCE0D4;
     `;
   return (
-    <div>
-        <Div2>
-        <Image src='chat.svg'/>
-        </Div2>
-    </div>
+    <Div>
+      <Div1>
+        <Login />
+      </Div1>
+      <Div2>
+        <Image src='chat.svg' />
+      </Div2>
+    </Div>
   )
 }
 
