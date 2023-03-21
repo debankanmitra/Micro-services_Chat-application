389d3933b8aa02b03d2578f0d6d5884e054bad9e
debankanmitra <debankanmitra@gmail.com>
2023-03-21 22:55:59 +0530

    messagescreen

--- chatApp/src/chat_system/Layoutgrid.jsx
+++ chatApp/src/chat_system/Layoutgrid.jsx
@@ -2,6 +2,7 @@ import React from 'react'
 import styled from 'styled-components'
 import Conversation from './Conversations'
 import Searchlist from './Searchlist'
+import Messagescreen from './Messagescreen'
 
 const Div = styled.body`
     display: grid;
@@ -12,23 +13,10 @@ const Top = styled.div`
     border: 2px solid black;
     grid-row: 1/2;
 `
-// const Searchlist = styled.div`
-//     border: 2px solid red;
-//     grid-row: 2/3;
-// `
-// const Conversations = styled.div`
-//     border: 2px solid blue;
-//     grid-row: 3/8;
-// `
 const Writemessage = styled.div `
     border: 2px solid green;
     grid-row: 7/8;
     height: 10vh;
-`
-const Messagescreen = styled.div `
-    border: 2px solid brown;
-    grid-row: 2/7;
-    margin: 10px;
 `
 const Usertop = styled.div `
     border: 2px solid violet;
@@ -40,7 +28,7 @@ function Layoutgrid() {
         <Top>top</Top>
         <Usertop>usertop</Usertop>
         <Searchlist/>
-        <Messagescreen>messagescreen</Messagescreen>
+        <Messagescreen/>
         {/* <Conversations><Conversation/></Conversations> */}
         <Conversation/>
         <Writemessage>writemesssage</Writemessage>
