9d2c5e74c8dcd93923ba5d83d5d7f862e4885b18
debankanmitra <debankanmitra@gmail.com>
2023-04-01 12:39:11 +0530

    final

--- chatApp/src/Writemessage/Emoji.jsx
+++ chatApp/src/Writemessage/Emoji.jsx
@@ -24,97 +24,113 @@ function Emojiicon() {
     )
 }
 
+const Emo = styled.div`
+    /* border: 1px solid black; */
+    padding: 2px 0px 2px 0px;
+    border-radius: 10px;
+    background: #ecf0f3;
+    width: 197px;
+    height: 140px;
+    overflow-y: scroll;
+    ::-webkit-scrollbar {
+    display: none;
+}
+    &>span{                           // the & selector represents the current selector
+        margin:4px;
+    }
+    &>span:hover{
+        background: #75b3e3;
+        cursor: pointer;
+    }
+`
 function Emoji() {
     return (
-        <div className='emoji'>
+        <Emo className='emoji'>
             <span>&#128512;</span>
-<span>&#128513;</span>
-<span>&#128514;</span>
-<span>&#128515;</span>
-<span>&#128516;</span>
-<span>&#128517;</span>
-<span>&#128518;</span>
-<span>&#128519;</span>
-<span>&#128520;</span>
-<span>&#128521;</span>
-<span>&#128522;</span>
-<span>&#128523;</span>
-<span>&#128524;</span>
-<span>&#128525;</span>
-<span>&#128526;</span>
-<span>&#128527;</span>
-<span>&#128528;</span>
-<span>&#128529;</span>
-<span>&#128530;</span>
-<span>&#128531;</span>
-<span>&#128532;</span>
-<span>&#128533;</span>
-<span>&#128534;</span>
-<span>&#128535;</span>
-<span>&#128536;</span>
-<span>&#128537;</span>
-<span>&#128538;</span>
-<span>&#128539;</span>
-<span>&#128540;</span>
-<span>&#128541;</span>
-<span>&#128542;</span>
-<span>&#128543;</span>
-<span>&#128544;</span>
-<span>&#128545;</span>
-<span>&#128546;</span>
-<span>&#128547;</span>
-<span>&#128548;</span>
-<span>&#128549;</span>
-<span>&#128550;</span>
-<span>&#128551;</span>
-<span>&#128552;</span>
-<span>&#128553;</span>
-<span>&#128554;</span>
-<span>&#128555;</span>
-<span>&#128556;</span>
-<span>&#128557;</span>
-<span>&#128558;</span>
-<span>&#128559;</span>
-<span>&#128560;</span>
-<span>&#128561;</span>
-<span>&#128562;</span>
-<span>&#128563;</span>
-<span>&#128564;</span>
-<span>&#128565;</span>
-<span>&#128566;</span>
-<span>&#128567;</span>
-<span>&#128568;</span>
-<span>&#128569;</span>
-<span>&#128570;</span>
-<span>&#128571;</span>
-<span>&#128572;</span>
-<span>&#128573;</span>
-<span>&#128574;</span>
-<span>&#128575;</span>
-<span>&#128576;</span>
-<span>&#128577;</span>
-<span>&#128578;</span>
-<span>&#128579;</span>
-<span>&#128580;</span>
-<span>&#128581;</span>
-<span>&#128582;</span>
-<span>&#128583;</span>
-<span>&#128584;</span>
-<span>&#128585;</span>
-<span>&#128586;</span>
-<span>&#128587;</span>
-<span>&#128588;</span>
-<span>&#128589;</span>
-<span>&#128590;</span>
-<span>&#128591;</span>
+            <span>&#128513;</span>
+            <span>&#128514;</span>
+            <span>&#128515;</span>
+            <span>&#128516;</span>
+            <span>&#128517;</span>
+            <span>&#128518;</span>
+            <span>&#128519;</span>
+            <span>&#128520;</span>
+            <span>&#128521;</span>
+            <span>&#128522;</span>
+            <span>&#128523;</span>
+            <span>&#128524;</span>
+            <span>&#128525;</span>
+            <span>&#128526;</span>
+            <span>&#128527;</span>
+            <span>&#128528;</span>
+            <span>&#128529;</span>
+            <span>&#128530;</span>
+            <span>&#128531;</span>
+            <span>&#128532;</span>
+            <span>&#128533;</span>
+            <span>&#128534;</span>
+            <span>&#128535;</span>
+            <span>&#128536;</span>
+            <span>&#128537;</span>
+            <span>&#128538;</span>
+            <span>&#128539;</span>
+            <span>&#128540;</span>
+            <span>&#128541;</span>
+            <span>&#128542;</span>
+            <span>&#128543;</span>
+            <span>&#128544;</span>
+            <span>&#128545;</span>
+            <span>&#128546;</span>
+            <span>&#128547;</span>
+            <span>&#128548;</span>
+            <span>&#128549;</span>
+            <span>&#128550;</span>
+            <span>&#128551;</span>
+            <span>&#128552;</span>
+            <span>&#128553;</span>
+            <span>&#128554;</span>
+            <span>&#128555;</span>
+            <span>&#128556;</span>
+            <span>&#128557;</span>
+            <span>&#128558;</span>
+            <span>&#128559;</span>
+            <span>&#128560;</span>
+            <span>&#128561;</span>
+            <span>&#128562;</span>
+            <span>&#128563;</span>
+            <span>&#128564;</span>
+            <span>&#128565;</span>
+            <span>&#128566;</span>
+            <span>&#128567;</span>
+            <span>&#128568;</span>
+            <span>&#128569;</span>
+            <span>&#128570;</span>
+            <span>&#128571;</span>
+            <span>&#128572;</span>
+            <span>&#128573;</span>
+            <span>&#128574;</span>
+            <span>&#128575;</span>
+            <span>&#128576;</span>
+            <span>&#128577;</span>
+            <span>&#128578;</span>
+            <span>&#128579;</span>
+            <span>&#128580;</span>
+            <span>&#128581;</span>
+            <span>&#128582;</span>
+            <span>&#128583;</span>
+            <span>&#128584;</span>
+            <span>&#128585;</span>
+            <span>&#128586;</span>
+            <span>&#128587;</span>
+            <span>&#128588;</span>
+            <span>&#128589;</span>
+            <span>&#128590;</span>
+            <span>&#128591;</span>
 
 
-
-
-
-</div>
+        </Emo>
     )
 }
 
 export default Emojiicon
-export {Emoji}
+export { Emoji }
