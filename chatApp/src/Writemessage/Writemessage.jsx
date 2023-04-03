005853fa33c1c89308cabfb5d4ccda348bd7855e
debankanmitra <debankanmitra@gmail.com>
2023-04-03 13:49:10 +0530

    send

--- chatApp/src/Writemessage/Writemessage.jsx
+++ chatApp/src/Writemessage/Writemessage.jsx
@@ -63,6 +63,8 @@ function Writemessage() {
     const [display, setDisplay] = useState('none');
     const [count, setCount] = useState(0);
     const [value,setvalue] = useState('');
+    const [view,setview] = useState("0 0 16 16");
+    const [path,setpath] = useState("M15.964.686a.5.5 0 0 0-.65-.65L.767 5.855H.766l-.452.18a.5.5 0 0 0-.082.887l.41.26l.001.002l4.995 3.178l3.178 4.995l.002.002l.26.41a.5.5 0 0 0 .886-.083l6-15Zm-1.833 1.89L6.637 10.07l-.215-.338a.5.5 0 0 0-.154-.154l-.338-.215l7.494-7.494l1.178-.471l-.47 1.178Z");
 
     const handleClick = () => {
         setCount(count + 1);
@@ -72,6 +74,15 @@ function Writemessage() {
 
     const addemojitotext=(e)=>{
         setvalue(value+e.target.textContent);
+        setview("0 0 24 24")
+        setpath("M12 2a3 3 0 0 1 3 3v6a3 3 0 0 1-3 3a3 3 0 0 1-3-3V5a3 3 0 0 1 3-3m7 9c0 3.53-2.61 6.44-6 6.93V21h-2v-3.07c-3.39-.49-6-3.4-6-6.93h2a5 5 0 0 0 5 5a5 5 0 0 0 5-5h2Z")
+    }
+
+    const inputchange = (e) => {
+        setvalue(e.target.value)
+        setview("0 0 24 24")
+        setpath("M12 2a3 3 0 0 1 3 3v6a3 3 0 0 1-3 3a3 3 0 0 1-3-3V5a3 3 0 0 1 3-3m7 9c0 3.53-2.61 6.44-6 6.93V21h-2v-3.07c-3.39-.49-6-3.4-6-6.93h2a5 5 0 0 0 5 5a5 5 0 0 0 5-5h2Z")
+        
     }
 
     return (
@@ -80,7 +91,7 @@ function Writemessage() {
             <Emojiicon onClick={handleClick} />
             <Emoji display={display} onClick={addemojitotext}/>
             <Div2>
-                <Input type="text" placeholder="Write Messages" value={value}  onChange={(e) => {setvalue(e.target.value)}}/>
+                <Input type="text" placeholder="Write Messages" value={value}  onChange={inputchange}/>
             </Div2>
 
             <Div3>
@@ -89,9 +100,8 @@ function Writemessage() {
             2.828 2.828l5.304-5.303a3.5 3.5 0 0 0-4.95-4.95L2.55 8a.5.5 0 1 0 .707.707Z"/></svg>
             </Div3>
             <Div4>
-                <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 16 16"><path fill="#0079FF"
-                    d="M15.964.686a.5.5 0 0 0-.65-.65L.767 5.855H.766l-.452.18a.5.5 0 0 0-.082.887l.41.26l.001.002l4.995 3.178l3.178 4.995l.002.002l.26.41a.5.5 0 0 0 .886-.083l6-15Zm-1.833 
-            1.89L6.637 10.07l-.215-.338a.5.5 0 0 0-.154-.154l-.338-.215l7.494-7.494l1.178-.471l-.47 1.178Z"/></svg>
+                <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox={view}><path fill="#0079FF"
+                    d={path}/></svg>
             </Div4>
         </Div>
     )
