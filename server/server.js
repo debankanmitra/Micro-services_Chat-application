796f949e7c2c15e532fafa5d8933266ef13725b7
debankanmitra <debankanmitra@gmail.com>
2023-04-11 23:21:18 +0530

    gitignore

---
+++ server/server.js
@@ -0,0 +1,24 @@
+const express = require('express');
+const app = express()
+const http = require("http").createServer(app);
+const cors = require("cors");
+app.use(cors());
+const {Server} = require("socket.io");
+const { Socket } = require('dgram');
+
+http.listen(8080, () => console.log("listening on port 8080"));
+
+const io = new Server(http,{
+    cors:{
+        origin: "http://localhost:3000",
+        methods: ["GET", "POST", "PUT"]
+    }
+})
+
+io.on("connection",Socket=> {
+    console.log("client connected",Socket.id);
+
+    Socket.on('disconnect',()=>{
+        console.log("user disconnected",Socket.id);
+    })
+})
