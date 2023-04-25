013db102ad4179150f9ecbf2fa29f47fcc9be236
debankanmitra <debankanmitra@gmail.com>
2023-04-26 01:24:37 +0530

    mdfied

--- chatApp/src/login_registration/auth.js
+++ chatApp/src/login_registration/auth.js
@@ -1,4 +1,3 @@
-// Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
 import { getAuth, signInWithPopup, GoogleAuthProvider } from "firebase/auth";
 
@@ -10,7 +9,7 @@ const firebaseConfig = {
     storageBucket: "login-with-17831.appspot.com",
     messagingSenderId: "734916142370",
     appId: "1:734916142370:web:2f484e1e9b70b4510d317b"
-  };
+};
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
@@ -19,14 +18,26 @@ const provider = new GoogleAuthProvider();
 
 export const signInWithGoogle = () => {
     signInWithPopup(auth, provider)
-        .then((result) => {
-            console.log(result);
+        .then(result => {
+            // This gives you a Google Access Token. You can use it to access the Google API.
+            const credential = GoogleAuthProvider.credentialFromResult(result);
+            const token = credential.accessToken;
+            // The signed-in user info.
+            const user = result.user;
+            // IdP data available using getAdditionalUserInfo(result)
+            // ...
+            console.log( user)
 
-        }).catch((error) => {
-
-            console.log(error);
-            
-
+        }).catch(error => {
+            // Handle Errors here.
+            const errorCode = error.code;
+            const errorMessage = error.message;
+            // The email of the user's account used.
+            const email = error.customData.email;
+            // The AuthCredential type that was used.
+            const credential = GoogleAuthProvider.credentialFromError(error);
+            // ...
+            console.log( errorCode, errorMessage, email)
         });
 
 }
