62b9a6ae09c7e4055f3a04f72ef07fe184062e50
debankanmitra <debankanmitra@gmail.com>
2023-06-18 22:03:35 +0530

    dgf

--- friends_list/pkg/models/model.go
+++ friends_list/pkg/models/model.go
@@ -10,34 +10,40 @@ var (
 	database *gorm.DB
 )
 
-type Friends struct {
+type Addpeoples struct {
 	ID       uint   `gorm:"primaryKey"`
 	Userid   string `gorm:"not null" json:"Userid"`
 	Friendid string `gorm:"not null" json:"Friendid"`
-	Name     string `gorm:"" json:"Name"`
-	Pic      string
-	Chatid   string `gorm:"not null" json:"Chatid"`
 }
+
+// type Friends struct {
+// 	ID       uint   `gorm:"primaryKey"`
+// 	Userid   string `gorm:"not null" json:"Userid"`
+// 	Friendid string `gorm:"not null" json:"Friendid"`
+// 	Name     string `gorm:"" json:"Name"`
+// 	Pic      string
+// 	Chatid   string `gorm:"not null" json:"Chatid"`
+// }
 
 func init() {
 	database = config.Connect()
-	database.AutoMigrate(&Friends{})
+	//database.AutoMigrate(&Friends{})
 }
 
 // CRUD functions of database
-func Read() *Friends {
-	var user *Friends
-	database.Find(&user)
-	return user
-}
+// func Read() *Friends {
+// 	var user *Friends
+// 	database.Find(&user)
+// 	return user
+// }
 
-func Create(user *Friends) *Friends { // it will be create bcuz we will be actually saving creating
+func Create(user *Addpeoples) *Addpeoples { // it will be create bcuz we will be actually saving creating
 	database.Create(user)
 	return user
 }
 
-func Delete(id string) *Friends { // this is fine too
-	var user *Friends
-	database.Where("Userid= ?", id).Delete(&user)
-	return user
-}
+// func Delete(id string) *Friends { // this is fine too
+// 	var user *Friends
+// 	database.Where("Userid= ?", id).Delete(&user)
+// 	return user
+// }
--- friends_list/pkg/routes/user-routes.go
+++ friends_list/pkg/routes/user-routes.go
@@ -9,9 +9,9 @@ import (
 var Route = func() *mux.Router {
 	router := mux.NewRouter()
 
-	router.HandleFunc("/api/connections", controller.GetConnections).Methods("GET")
+	//router.HandleFunc("/api/connections", controller.GetConnections).Methods("GET")
 	router.HandleFunc("/api/add", controller.AddConnection).Methods("POST")
-	router.HandleFunc("/api/connections/{id}", controller.DeleteConnection).Methods("DELETE")
+	//router.HandleFunc("/api/connections/{id}", controller.DeleteConnection).Methods("DELETE")
 
 	return router
 }
