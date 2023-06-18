8da51bf5f8ec36081a12a5e4a95fb6af166b39e2
debankanmitra <debankanmitra@gmail.com>
2023-06-19 00:18:44 +0530

    sql

--- friends_list/pkg/models/model.go
+++ friends_list/pkg/models/model.go
@@ -16,14 +16,22 @@ type Addpeoples struct {
 	Friendid string `gorm:"not null" json:"Friendid"`
 }
 
-// type Friends struct {
-// 	ID       uint   `gorm:"primaryKey"`
-// 	Userid   string `gorm:"not null" json:"Userid"`
-// 	Friendid string `gorm:"not null" json:"Friendid"`
-// 	Name     string `gorm:"" json:"Name"`
-// 	Pic      string
-// 	Chatid   string `gorm:"not null" json:"Chatid"`
-// }
+type User struct {
+	ID    uint   `gorm:"primaryKey"`
+	Uuid  string `gorm:"not null" json:"Uuid"`
+	Name  string `gorm:"" json:"Name"`
+	Pic   string
+	Email string `gorm:"unique;not null" json:"Email"`
+}
+
+type Friends struct {
+	ID       uint   `gorm:"primaryKey"`
+	Userid   string `gorm:"not null" json:"Userid"`
+	Friendid string `gorm:"not null" json:"Friendid"`
+	Name     string `gorm:"" json:"Name"`
+	Pic      string
+	Chatid   string `gorm:"not null" json:"Chatid"`
+}
 
 func init() {
 	database = config.Connect()
@@ -37,9 +45,13 @@ func init() {
 // 	return user
 // }
 
-func Create(user *Addpeoples) *Addpeoples { // it will be create bcuz we will be actually saving creating
+func Create(user *Addpeoples) *Friends { // it will be create bcuz we will be actually saving creating
 	database.Create(user)
-	return user
+	var friends *Friends
+	database.Table("users").Select("users.uuid,addpeoples.Friendid,users.name,users.Pic,addpeoples.Userid").Joins("join addpeoples on addpeoples.Friendid = users.uuid").Scan(&friends)
+
+	database.Create(friends)
+	return friends
 }
 
 // func Delete(id string) *Friends { // this is fine too
